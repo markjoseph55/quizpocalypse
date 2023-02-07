1d97524906f7a31504647e60409c5fbc7ecec81f
Ma. Angela Janelle D.P. Fabricante <angelajanelle54@gmail.com>
2023-02-07 10:02:14 +0800

    Add quizpocalypse

--- js/store.js
+++ js/store.js
@@ -1,15 +1,12 @@
 // modules
 import arvic from './modules/arvic.js';
+import angela from './modules/angela.js';
 
-// import your .js file here (e.g. import john from './modules/john.js')
-// ...
 
 const store = Vuex.createStore({
     modules: {
         arvic,
-
-        // include your module here (e.g. john)
-        // ...
+        angela,
 
     }
 });
