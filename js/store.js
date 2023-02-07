2835dbb6464d5fed04046df7442f4c9daaaf2b9b
iMAC 8 MAC Lab <louvasquez@my.cspc.edu.ph>
2023-02-07 10:37:51 +0800

    add lourdes

--- js/store.js
+++ js/store.js
@@ -1,12 +1,12 @@
 // modules
-import arvic from './modules/arvic.js';
+import lourdes from './modules/lourdes.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
 // ...
 
 const store = Vuex.createStore({
     modules: {
-        arvic,
+        lourdes,
 
         // include your module here (e.g. john)
         // ...
