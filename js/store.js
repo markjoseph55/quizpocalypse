6e1f5b51d5d92ea7d8bc6ef81a4b8bf7b7b99ae8
imac16 <122844582+juneljoven@users.noreply.github.com>
2023-02-07 10:05:06 +0800

    Add quizApocalypse
    Junel R. Joven
    BSIT3B

--- js/store.js
+++ js/store.js
@@ -1,12 +1,12 @@
 // modules
-import arvic from './modules/arvic.js';
+import junel from './modules/junel.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
 // ...
 
 const store = Vuex.createStore({
     modules: {
-        arvic,
+        junel,
 
         // include your module here (e.g. john)
         // ...
