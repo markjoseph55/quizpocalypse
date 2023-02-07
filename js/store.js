0553ec6b63fd32b152dd3ef1a18488f15effdc11
Arjay4 <arjbascon@my.cspc.edu.ph>
2023-02-07 10:22:55 +0800

    Add quizpocalypse
    Arjay Bascon
    BSIT3B

--- js/store.js
+++ js/store.js
@@ -2,11 +2,12 @@
 import arvic from './modules/arvic.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
-// ...
+import arjay from './modules/arjay.js';
+
 
 const store = Vuex.createStore({
     modules: {
-        arvic,
+        arvic,arjay
 
         // include your module here (e.g. john)
         // ...
