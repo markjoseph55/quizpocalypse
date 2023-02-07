e57012513f09ba240cd682a48af8753b415ebc5b
kathycode <katramos@my.cspc.edu.ph>
2023-02-07 10:45:01 +0800

    Add quizpocalypse
    Kathlyn A. Ramos

--- js/store.js
+++ js/store.js
@@ -1,12 +1,12 @@
 // modules
-import arvic from './modules/arvic.js';
+import kathlyn from './modules/kathlyn.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
 // ...
 
 const store = Vuex.createStore({
     modules: {
-        arvic,
+        kathlyn,
 
         // include your module here (e.g. john)
         // ...
