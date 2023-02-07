6c75077bed198180d0e464a1e4c264e1f1b2e509
imac38 <122961904+ShayApondar@users.noreply.github.com>
2023-02-07 10:16:00 +0800

    add shaina 10 items quiz

--- js/store.js
+++ js/store.js
@@ -1,15 +1,15 @@
 // modules
 import arvic from './modules/arvic.js';
+import shaina from './modules/shaina.js';
 
-// import your .js file here (e.g. import john from './modules/john.js')
-// ...
+
 
 const store = Vuex.createStore({
     modules: {
         arvic,
 
-        // include your module here (e.g. john)
-        // ...
+        shaina,
+
 
     }
 });
