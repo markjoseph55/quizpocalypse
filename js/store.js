e73ea2aed50aba26c24e0d2d2ad61f2a9a2aa10d
Ma. Lourdes Camano <mlcamano@my.cspc.edu.ph>
2023-02-07 09:54:58 +0800

    Add Ma. Lourdes Camano

--- js/store.js
+++ js/store.js
@@ -1,12 +1,12 @@
 // modules
-import arvic from './modules/arvic.js';
+import lourdescamano from './modules/lourdescamano.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
 // ...
 
 const store = Vuex.createStore({
     modules: {
-        arvic,
+        lourdescamano,
 
         // include your module here (e.g. john)
         // ...
