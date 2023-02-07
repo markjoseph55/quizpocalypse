76e5e5ae6172824431541cb8e9e9baef906b7b11
Francisca Mae D. Apolinar <fraapolinar@my.cspc.edu.ph>
2023-02-07 10:04:24 +0800

    quizapocalypse
    Mark Louis R. Odavar

--- js/store.js
+++ js/store.js
@@ -1,12 +1,13 @@
 // modules
 import arvic from './modules/arvic.js';
+import marklouis from './modules/marklouis.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
 // ...
 
 const store = Vuex.createStore({
     modules: {
-        arvic,
+        marklouis,arvic
 
         // include your module here (e.g. john)
         // ...
