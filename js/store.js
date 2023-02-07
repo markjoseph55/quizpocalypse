17f3dff8abc03a996d66a281bb3c854b881e202f
iMac 14 <cspc>
2023-02-07 10:04:19 +0800

    Add quizpocalypse
    Craig Ben L. Cadag
    BSIT 3B

--- js/store.js
+++ js/store.js
@@ -4,12 +4,15 @@ import arvic from './modules/arvic.js';
 // import your .js file here (e.g. import john from './modules/john.js')
 // ...
 
+import craig from './modules/Craig.js';
+
 const store = Vuex.createStore({
     modules: {
         arvic,
 
         // include your module here (e.g. john)
         // ...
+        craig,
 
     }
 });
