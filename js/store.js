8e474d964882b5105b6236e20b7d7f9ffd7d0de6
kulotsystems <kulotsystems@gmail.com>
2023-02-08 20:53:34 +0800

    Remove arvic from list.

--- js/store.js
+++ js/store.js
@@ -1,5 +1,5 @@
 // modules
-import arvic from './modules/arvic.js';
+// import arvic from './modules/arvic.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
 // ...
@@ -25,7 +25,7 @@ import kathlyn from './modules/kathlyn.js';
 
 const store = Vuex.createStore({
     modules: {
-        arvic,
+        // arvic,
 
         // include your module here (e.g. john)
         // ...
